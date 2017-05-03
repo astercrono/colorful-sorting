f4b1c16316d5f688c9a27fa56922fddb3266d127
Travis Kern <kern.travis@gmail.com>
2017-05-03 08:54:28 -0400

    Cleaned up test
    Moved main test logic to its own function which has the algorithm to
    test injected into it.

--- src/test/java/net/astercrono/sorting/SortTest.java
+++ src/test/java/net/astercrono/sorting/SortTest.java
@@ -6,22 +6,20 @@ import org.junit.Test;
 public class SortTest {
 	@Test
 	public void testMergeSort() {
-		final Integer[] unsorted = new Integer[] { 3, 4, 1, 2, 5 };
-		final Integer[] expected = new Integer[] { 1, 2, 3, 4, 5 };
-
 		final HistoricalSort sort = new HistoricalMergeSort();
-		final SortHistory history = sort.sort(unsorted);
-		final Integer[] sorted = history.getSortedList();
-
-		Assert.assertArrayEquals(expected, sorted);
+		testSort(sort);
 	}
 
 	@Test
 	public void testQuickSort() {
+		final HistoricalSort sort = new HistoricalQuickSort();
+		testSort(sort);
+	}
+
+	private void testSort(final HistoricalSort sort) {
 		final Integer[] unsorted = new Integer[] { 3, 4, 1, 2, 5 };
 		final Integer[] expected = new Integer[] { 1, 2, 3, 4, 5 };
 
-		final HistoricalSort sort = new HistoricalQuickSort();
 		final SortHistory history = sort.sort(unsorted);
 		final Integer[] sorted = history.getSortedList();
 
